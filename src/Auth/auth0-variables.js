001d72c1b5117e34bcdf028d534c5d168140a53a
riccardone <riccardo@dinuzzo.it>
2017-12-05 23:10:05 +0000

    dockerised the app

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,5 +1,6 @@
 export const AUTH_CONFIG = {
   domain: 'myselflog.eu.auth0.com',
-  clientId: 'yqRCVaMeuSwGiSwHdKKa336iOc09b7v2',
+  clientId: 'yqRCVaMeuSwGiSwHdKKa336iOc09b7v2',  
+  // callbackUrl: 'http://178.62.84.102/callback'
   callbackUrl: 'http://localhost:3000/callback'
 }
