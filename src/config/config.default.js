ae334ec5e4d556afe03c10b12921a01c01e52dfd
riccardone <riccardo@dinuzzo.it>
2018-02-04 13:20:25 +0000

    config changes

--- src/config/config.default.js
+++ src/config/config.default.js
@@ -2,7 +2,7 @@ var config = module.exports = {};
 
 config.env = 'dev';
 
-config.apiLink = 'http://myselflog-api:5001';
+config.apiLink = 'http://api.myselflog.com:5001';
 config.path = '/api/v1/logs';
 config.callbackUrl = "http://preview.myselflog.com/callback";
 
