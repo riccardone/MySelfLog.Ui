247dce003502eb1b78d10fdbb5a92ecd0f62b03a
riccardone <riccardo@dinuzzo.it>
2018-02-04 12:52:23 +0000

    config changes

--- src/config/config.default.js
+++ src/config/config.default.js
@@ -1,12 +1,10 @@
 var config = module.exports = {};
 
 config.env = 'dev';
-config.host = 'localhost';
-config.port = 3001;
 
-config.apiLink = 'http://localhost:3001';
+config.apiLink = 'http://myselflog-api:3001';
 config.path = '/api/v1/logs';
-config.callbackUrl = "http://localhost:3000/callback";
+config.callbackUrl = "http://preview.myselflog.com/callback";
 
 // logging
 config.logAppender = "debug";
--- src/config/config.production.js
+++ src/config/config.production.js
@@ -1,15 +1,3 @@
 var config = require('./config.default');
 
-config.env = 'production';
-config.host = 'localhost';
-config.port = 3001;
-
-config.eventstoreConnection = 'http://infrastructure.myselflog.com:2113';
-config.publishTo = "diary-input";
-config.callbackUrl = "http://preview.myselflog.com/callback";
-
-// logging
-config.logAppender = "error";
-config.logLevel = "error";
-
 module.exports = config;
