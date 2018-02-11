6c78472da30a54071923139acab498ed8c6a0722
riccardone <riccardo@dinuzzo.it>
2018-02-11 22:24:29 +0000

    config changes

--- src/config/config.production.js
+++ src/config/config.production.js
@@ -2,7 +2,7 @@ var config = require('./config.default');
 
 config.env = 'production';
 
-config.apiLink = 'http://api.myselflog.com:5001';
+config.apiLink = 'http://preview.myselflog.com:5001';
 config.path = '/api/v1/logs';
 config.callbackUrl = "http://preview.myselflog.com/callback";
 
