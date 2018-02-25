9fda374442539e0c0634dbe526d6febea2df3d59
riccardone <riccardo@dinuzzo.it>
2018-02-25 10:49:54 +0000

    removed useless configs

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,7 +1,5 @@
-var cfg = require('../config');
-
 export const AUTH_CONFIG = {
   domain: 'myselflog.eu.auth0.com',
   clientId: 'yqRCVaMeuSwGiSwHdKKa336iOc09b7v2',    
-  callbackUrl: cfg.callbackUrl
+  callbackUrl: process.env.REACT_APP_NODE_ENV == 'dev' ? 'http://localhost:3000/callback' : 'http://preview.myselflog.com/callback'
 }
