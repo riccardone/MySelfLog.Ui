ee37e0a0fff1c6f4c8f130867891a148e1599666
riccardone <riccardo@dinuzzo.it>
2018-03-02 15:39:35 +0000

    removed dotenv and fixed configs

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'myselflog.eu.auth0.com',
   clientId: 'yqRCVaMeuSwGiSwHdKKa336iOc09b7v2',    
-  callbackUrl: process.env.REACT_APP_NODE_ENV == 'dev' ? 'http://localhost:3000/callback' : 'http://preview.myselflog.com/callback'
-}
+  callbackUrl: process.env.NODE_ENV === 'production' ? 'http://preview.myselflog.com/callback' : 'http://localhost:3000/callback'
+};
