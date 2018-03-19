aff1eb075251323170d74da1894f95eb02046da5
riccardone <riccardo@dinuzzo.it>
2018-03-19 11:41:37 +0000

    minor fixes

--- src/footer.js
+++ src/footer.js
@@ -8,8 +8,7 @@ class Footer extends Component {
       <footer className="navbar-fixed-bottom footer">
 					<div className="container">
 						<div className="row">
-							<p>@2018 <a href="http://www.dinuzzo.co.uk/" title="open my personal website">Riccardo Di Nuzzo</a></p>
-              <p><a href="https://github.com/riccardone/MySelfLog.Ui" title="source code on GitHub"><img src="GitHub-Mark-32px.png" alt="source code on GitHub" /></a></p>
+							<p>@2018 <a href="http://www.dinuzzo.co.uk/" title="open my personal website">Riccardo Di Nuzzo</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://github.com/riccardone/MySelfLog.Ui" title="source code on GitHub"><img src="GitHub-Mark-32px.png" alt="source code on GitHub" /></a></p>              
 						</div>
 					</div>
 				</footer>
