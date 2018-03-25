00bab3a1f6b5cae288c04d77b1c15af92bc0a7f6
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2018-03-25 18:26:18 +0100

    renamed back app -> index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import './app.css';
+import './index.css';
 import { makeMainRoutes } from './routes';
 
 const routes = makeMainRoutes();
